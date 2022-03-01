dd0ae66a54f4dac172ab1ce8a27c1154c18f15ed
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-02 00:13:11 +0600

    again add display result

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const displayResult = document.getElementById("display-result");
 const searchPhone = () => {
     const inputField = document.getElementById("input-text");
     const error = document.getElementById("error");
@@ -5,8 +6,13 @@ const searchPhone = () => {
     inputField.value = "";
     // Error Massage
     if (!isNaN(inputValue)) {
-        error.innerText = "Please Type Only Mobile Name";
+        error.innerText = "Type Phone Brand Name";
+        displayResult.innerHTML = "";
     }
+    // else if (inputValue) {
+    //     error.innerText = "Please Type Again";
+    //     displayResult.innerHTML = "";
+    // }
     else {
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
         fetch(url)
@@ -14,27 +20,27 @@ const searchPhone = () => {
             .then(data => firstData(data))
         error.innerText = "";
     }
+    displayResult.innerHTML = "";
 
 };
 
 const firstData = value => {
     // console.log(result);
     const values = value.data;
-    const searchResult = document.getElementById("load-data");
     for (const value of values) {
         const div = document.createElement("div");
+        div.classList.add("col-lg-4")
+        div.classList.add("mb-5")
         div.innerHTML = `
-        <div class="container w-50 shadow-lg p-3 mb-5 bg-body rounded">
-        <img src="${value.image}" class="card-img-top" alt="...">
-        <h5 class="card-title m-2">Name: ${value.phone_name}</h5>
-        <p class="card-text m-2">Brand: ${value.brand}</p>
-        <div class="d-grid gap-2 col-6 mx-auto">
-        <button class="btn btn-primary" type="button">Button</button>
-        </div>
-        </div>    
+        <div class="card mx-auto mt-5 p-3 shadow p-3 mb-5 bg-body rounded-3" style="width: 15rem;">
+  <img src="${value.image}" height="200" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Name: ${value.phone_name}</h5>
+    <h5 class="card-text">Brand: ${value.brand}</h5>
+    <a href="#" class="btn btn-primary">Mobile Details</a>
+  </div>
+</div>    
         `;
-        searchResult.appendChild(div);
-
+        displayResult.appendChild(div);
     }
-
 };
