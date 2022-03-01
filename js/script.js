aef650eeefb623d5c60b588c747241c3675072fc
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-02 04:18:37 +0600

    add show details

--- js/script.js
+++ js/script.js
@@ -17,30 +17,56 @@ const searchPhone = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
         fetch(url)
             .then(response => response.json())
-            .then(data => firstData(data))
+            .then(data => result(data.data))
         error.innerText = "";
     }
     displayResult.innerHTML = "";
 
 };
 
-const firstData = value => {
-    // console.log(result);
-    const values = value.data;
+const result = values => {
     for (const value of values) {
         const div = document.createElement("div");
         div.classList.add("col-lg-4")
         div.classList.add("mb-5")
         div.innerHTML = `
         <div class="card mx-auto mt-5 p-3 shadow p-3 mb-5 bg-body rounded-3" style="width: 15rem;">
-  <img src="${value.image}" height="200" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Name: ${value.phone_name}</h5>
-    <h5 class="card-text">Brand: ${value.brand}</h5>
-    <a href="#" class="btn btn-primary">Mobile Details</a>
-  </div>
-</div>    
+        <img src="${value.image}" height="200" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Name: ${value.phone_name}</h5>
+        <h5 class="card-text">Brand: ${value.brand}</h5>
+        <button onclick="mobileDetails('${value.slug}')" class="btn btn-primary mt-2">Mobile Details</button>
+        </div>
+        </div>    
         `;
         displayResult.appendChild(div);
     }
 };
+
+const mobileDetails = details => {
+    const url = `https://openapi.programming-hero.com/api/phone/${details}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetails(data.data))
+};
+
+const showDetails = details => {
+    console.log(details);
+    document.getElementById("display-details").innerHTML = `
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row">
+      <div class="col-md-4">
+        <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Memory: ${details.memory}</h5>
+          <p class="card-text">Storage: ${details.storage} </p>
+          <p class="card-text">Release Date: ${details.releaseDate}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+}
+
